a567bab6bd1343f5e806d58da7cf1da6c24e051a
rlizm100 <rlizm100@cox.net>
2020-08-14 20:35:01 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
+/*var connection;
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -11,8 +11,26 @@ if (process.env.JAWSDB_URL){
     password: 'hacktheplanet',
     database: 'todoagain_db'
   })
-}
+}*/
 
-connection.connect();
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "adoptme_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+// connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
